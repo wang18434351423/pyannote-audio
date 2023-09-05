661582042f532b3ad2e02ff8086a4ff9c1b73339
kiki <48913079+wang18434351423@users.noreply.github.com>
2023-09-05 16:26:24 +0800

    Create test_audio_same.py

---
+++ tests/test_audio_same.py
@@ -0,0 +1,14 @@
+from pyannote.audio import Pipeline
+  
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
+
+# inference on the whole file
+pipeline("file.wav")
+
+# inference on an excerpt
+from pyannote.core import Segment
+excerpt = Segment(start=2.0, end=5.0)
+
+from pyannote.audio import Audio
+waveform, sample_rate = Audio().crop("file.wav", excerpt)
+pipeline({"waveform": waveform, "sample_rate": sample_rate})
